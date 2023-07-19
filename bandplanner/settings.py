fde2fbcaa2891e9d08c03d2c8dceaee645633a88
Jordan Victoria <jgomeskuehner@gmail.com>
2023-07-19 13:25:10 -0500

    removed slash

--- bandplanner/settings.py
+++ bandplanner/settings.py
@@ -63,7 +63,7 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
-    'https://band-planner-9b2a3c99e7e8.herokuapp.com/'
+    'https://band-planner-9b2a3c99e7e8.herokuapp.com'
 )
 
 MIDDLEWARE = [
